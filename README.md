b6586c04d2e4ef9818de919c43370e950f43a964
serio.shi <lionsmail1015@gmail.com>
2018-01-05 15:06:18 +0800

    Amend someting

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ebbinghaus
 ebbinghaus forgetting curve
+
+### 自动生成计划
+[点击查看](http://fantasylion.github.io/english/How-to-remember-sthing)
--- ebbinghaus.py
+++ ebbinghaus.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+def printPlanOneDay( days, time, forget ):
+    forgetTime = [2, 4, 7, 15]
+    print("第", days + 1, "天", "     list", time + 1, end=' ')
+    for forget in forgetTime:
+        if ((time + 1) > forget - 1):
+            print("     *list", ((time + 1) - (forget - 1)), end=' ')
+    print("     *list", time + 1)
+
 def getPlan( listNum ):
-    forgetTime = {2, 4, 7, 15}
 
     print("			新学		背诵")
     forgetTime = [2, 4, 7, 15]
     days = 0
     forget = 0
     while days < listNum:
-        print("第", days + 1, "天", "     list", days + 1, end=' ')
+        printPlanOneDay(days,days, forget)
         days += 1
-        for forget in forgetTime:
-            if ((days + 1) > forget - 1):
-                print("     *list", ((days + 1) - (forget - 1)), end=' ')
-        print("     *list", days + 1)
+    lastTime = listNum + 15
+    while lastTime >= 0:
+        printPlanOneDay(days, lastTime, forget)
+        lastTime -= 1
+        days += 1
 
 
 
